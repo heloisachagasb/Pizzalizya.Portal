cd8cc66274767dfe92d498ad3c9bc3a6360c463a
Heloisa Chagas Bezerra <heloisachagasbezerra@gmail.com>
2025-11-07 23:04:46 -0300

    correção

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,64 +3,6 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  lizyBaseUrlNucleo: 'https://lizy-nucleo-qa.azurewebsites.net/api/v1',
-  lizyBaseUrlProdutos: 'https://lizy-produtos-qa.azurewebsites.net/api/v1',
-  lizyBaseUrlParceiros: 'https://lizy-parceiros-qa.azurewebsites.net/api/v1/',
-  lizyBaseUrlRequisicoesCompras:
-    'https://lizy-compras-qa.azurewebsites.net/api/v1/requisicoes',
-  lizyBaseUrlRequisicoesEstoque:
-    'https://lizy-compras-qa.azurewebsites.net/api/v1/requisicoes-estoque',
-  lizyBaseUrlPedidosCompras:
-    'https://lizy-compras-qa.azurewebsites.net/api/v1/pedidos',
-  lizyBaseUrlEscopoServicos:
-    'https://lizy-servicos-qa.azurewebsites.net/api/v1/escopo',
-  lizyBancosApiUrl: 'https://lizy-bancos-qa.azurewebsites.net/api/v1/bancos',
-  lizyComprasApiUrl: 'https://lizy-compras-qa.azurewebsites.net/api/v1/compras',
-  lizyEmpresasApiUrl:
-    'https://lizy-empresas-qa.azurewebsites.net/api/v1/empresas',
-  lizyEstoqueApiUrl: 'https://lizy-estoque-qa.azurewebsites.net/api/v1/estoque',
-  lizyFaturamentoApiUrl:
-    'https://lizy-faturamento-qa.azurewebsites.net/api/v1/faturamento',
-  lizyFinanceiroApiUrl:
-    'https://lizy-financeiro-qa.azurewebsites.net/api/v1/financeiro/lancamentos',
-  lizyIdentidadeApiUrl:
-    'https://lizy-identidade-qa.azurewebsites.net/api/v1/identidade/',
-  lizyNucleoApiUrl: 'https://lizy-nucleo-qa.azurewebsites.net/api/nucleo/v1/',
-  lizyParceirosApiUrl:
-    'https://lizy-parceiros-qa.azurewebsites.net/api/v1/parceiros',
-  lizyNucleoErpApiUrl:
-    'https://lizy-nucleo-erp-qa.azurewebsites.net/api/v1/nucleo-erp',
-  lizyProdutosApiUrl:
-    'https://lizy-produtos-qa.azurewebsites.net/api/v1/produtos',
-  lizyGrupoProdutosApiUrl:
-    'https://lizy-produtos-qa.azurewebsites.net/api/produtos/v1/',
-  lizyServicosApiUrl:
-    'https://lizy-servicos-qa.azurewebsites.net/api/v1/servicos',
-  lizyLocalEstoqueApiUrl:
-    'https://lizy-nucleo-qa.azurewebsites.net/api/nucleo/v1/localestoque',
-  lizyVendasApiUrl: 'https://lizy-vendas-qa.azurewebsites.net/api/v1/vendas',
-  lizyOrcamentosApiUrl:
-    'https://lizy-vendas-qa.azurewebsites.net/api/v1/orcamentos',
-  lizyFollowUpApiUrl:
-    'https://lizy-vendas-qa.azurewebsites.net/api/v1/followuporcamentos',
-  lizyCamposPersonalizadosApiUrl:
-    'https://lizy-vendas-qa.azurewebsites.net/api/v1/campos-personalizados',
-  lizyTipoAgendamentoApiUrl:
-    'https://lizy-parceiros-qa.azurewebsites.net/api/v1/tipo-agendamento',
-  lizyOutrosCustosApiUrl:
-    'https://lizy-vendas-qa.azurewebsites.net/api/v1/outros-custos',
-  lizyBaseFinanceiroApiUrl:
-    'https://lizy-financeiro-qa.azurewebsites.net/api/v1/financeiro',
-  lizyAlcadaDeComprasApiUrl:
-    'https://lizy-compras-qa.azurewebsites.net/api/v1/alcada-de-compras',
-  lizyControleProducaoApiUrl:
-    'https://lizy-controle-producao-qa.azurewebsites.net/api/v1/controle-producao',
-  lizyBuilderApiUrl:
-    'https://lizy-builder-gydecya9esbxgwbg.brazilsouth-01.azurewebsites.net/api',
-  identityServerUrl:
-    'https://lizybuilderapi-f2bvb3djcjb0hzbe.brazilsouth-01.azurewebsites.net',
-  lizyBuilderCrudsApiUrl:
-    'https://lizybuilderapi-f2bvb3djcjb0hzbe.brazilsouth-01.azurewebsites.net/api/v2/',
 };
 
 /*
